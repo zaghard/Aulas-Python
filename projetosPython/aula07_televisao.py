90470c0ebf225f78a5732c77b1689e5ead0eb17a
Mauricio G. Almeida <mauricioalmeida.info@gmail.com>
2020-09-07 22:03:11 -0300

    alteração da funcao power,canal

--- projetosPython/aula07_televisao.py
+++ projetosPython/aula07_televisao.py
@@ -1,1 +1,32 @@
-print()
+class Televisao:
+    def __init__(self):
+        self.ligada = False
+        self.canal = 5
+
+    def power(self):
+        if self.ligada:
+            self.ligada = False
+        else:
+            self.ligada = True
+
+    def aumenta_canal(self):
+        if self.ligada:
+            self.canal += 1
+    def dimini_canal(self):
+        if self.ligada:
+            self.canal -= 1
+
+
+televisao = Televisao()
+print('Televisao está ligada: "{}"'.format(televisao.ligada))
+televisao.power()
+print('Televisao está ligada: "{}"'.format(televisao.ligada))
+televisao.power()
+print('Televisao está ligada: "{}"'.format(televisao.ligada))
+televisao.power()
+print('Canal:"{}"'.format(televisao.canal))
+televisao.aumenta_canal()
+televisao.aumenta_canal()
+print('Canal:"{}"'.format(televisao.canal))
+televisao.dimini_canal()
+print('Canal:"{}"'.format(televisao.canal))
