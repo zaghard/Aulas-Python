d9859925c2c0e23ca26115be6adb7ec8da52f0b9
Mauricio G. Almeida <mauricioalmeida.info@gmail.com>
2024-02-09 00:17:51 -0300

    inicio das aulas

--- projetosPython/aula12.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-def retorna_dados_cep(cep):
-    response = requests.get('https://viacep.com.br/ws/{}/json/'.format(cep))
-    print(response.status_code)
-    print(response.json())
-    dados_cep = response.json()
-    print(dados_cep['logradouro'])
-    
